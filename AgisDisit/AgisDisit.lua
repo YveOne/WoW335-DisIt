7461c7874c5b650fc485cb031c7ddc8a3a3ff5b4
Yvonne P <agita@live.de>
2017-06-05 20:05:25 +0200

    Add files via upload

---
+++ AgisDisit/AgisDisit.lua
@@ -0,0 +1,137 @@
+
+-----------------------------------------------------------------------------------
+local ADDONNAME, THIS = ...;
+-----------------------------------------------------------------------------------
+
+-- localize
+
+local GetContainerNumSlots, GetContainerItemInfo, GetItemInfo = GetContainerNumSlots, GetContainerItemInfo, GetItemInfo
+local print, tonumber, strmatch, strfind, strtrim = print, tonumber, strmatch, strfind, strtrim
+local HasAction, GetActionInfo = HasAction, GetActionInfo
+local GetMacroBody, GetMacroInfo, EditMacro = GetMacroBody, GetMacroInfo, EditMacro
+local CloseLoot = CloseLoot
+local IsFlying, IsFalling = IsFlying, IsFalling
+local UnitAffectingCombat, UnitCastingInfo, UnitIsDead = UnitAffectingCombat, UnitCastingInfo, UnitIsDead
+
+-- variables
+
+local CloseNextLoot = false
+local SpellNameDE = GetSpellInfo(13262)
+
+-----------------------------------------------------------------------------------
+
+local function findItemID()
+    local i, j, _, locked, itemLink, itemName, itemRarity, itemLevel, equipLoc, sellPrice, itemID
+    for i=0,4 do
+        for j=1,GetContainerNumSlots(i) do
+            _, _, locked, _, _, _, itemLink = GetContainerItemInfo(i, j)
+            if ( itemLink ) then
+                itemName, itemLink, itemRarity, itemLevel, _, _, _, _, equipLoc, _, sellPrice = GetItemInfo(itemLink)
+                if ( itemRarity == 2 or itemRarity == 3 ) then
+                    if ( equipLoc ~= "" and sellPrice > 0 ) then
+                        itemID, _ = strmatch(itemLink, "item%:(%d+)%:.+%[(.-)%]")
+                        return itemID + 0
+                    end
+                end
+            end
+        end
+    end
+    return nil
+end
+
+-----------------------------------------------------------------------------------
+
+local function findSlotInfo(text)
+	local slotID, slotType, macroID;
+	for slotID=1,120 do
+        if ( HasAction(slotID) ) then
+            slotType, macroID = GetActionInfo(slotID)
+            if ( slotType == "macro" ) then
+                if ( strfind(GetMacroBody(macroID), text) ) then
+                    return macroID, slotID, ceil(slotID/12), slotID % 12
+                end
+            end
+        end
+    end
+    return nil
+end
+
+-----------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------
+local EventHandlers = {}
+-----------------------------------------------------------------------------------
+
+function EventHandlers:UNIT_SPELLCAST_SUCCEEDED(unitID, spell)
+    if ( unitID == "player" and spell == SpellNameDE ) then
+        CloseNextLoot = true
+    end
+end
+
+function EventHandlers:LOOT_OPENED(auto)
+    if ( CloseNextLoot ) then
+        CloseNextLoot = false
+        CloseLoot()
+    end
+end
+
+--function EventHandlers:UNIT_SPELLCAST_INTERRUPTED(unitID, spell)
+--    if ( unitID == "player" and spell == SpellNameDE ) then
+--    end
+--end
+
+-----------------------------------------------------------------------------------
+
+local EventFrame = CreateFrame("FRAME");
+function EventFrame:OnEvent(event, ...)
+    EventHandlers[event](self, ...)
+end
+EventFrame:SetScript("OnEvent", EventFrame.OnEvent);
+for event,v in pairs(EventHandlers) do
+    EventFrame:RegisterEvent(event);
+end
+
+-----------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------
+
+SLASH_AGIDISIT1 = "/disit";
+SlashCmdList["AGIDISIT"] = function()
+
+    local macroID = findSlotInfo("/disit")
+    if ( macroID == nil ) then
+        print("cannot find action slot")
+        return
+    end
+
+    local macroName, macroIcon, macroBody, macroLocal = GetMacroInfo(macroID)
+    macroBody = strtrim(strmatch(macroBody, "(.+/disit).*"))
+
+    if ( IsFlying() or IsFalling() or UnitAffectingCombat("player") or UnitCastingInfo("player") or UnitIsDead("player") ) then
+        print("Cant disit right now")
+    else
+        local itemID = findItemID()
+        if ( itemID ) then
+            macroBody = macroBody .. "\n/use " .. SpellNameDE .. "\n/use item:" .. itemID .. "\n/dised " .. macroID
+            local itemName, itemLink = GetItemInfo(itemID)
+            print("DISENCHANTING "..itemLink)
+        else
+            print("no items found for disenchanting")
+        end
+    end
+
+    EditMacro(macroID, macroName, macroIcon, macroBody, macroLocal)
+
+end
+
+-----------------------------------------------------------------------------------
+
+SLASH_AGIDISED1 = "/dised";
+SlashCmdList["AGIDISED"] = function(macroID)
+    macroID = tonumber(macroID)
+    local macroName, macroIcon, macroBody, macroLocal = GetMacroInfo(macroID)
+    macroBody = strtrim(strmatch(macroBody, "(.*/disit).*"))
+    EditMacro(macroID, macroName, macroIcon, macroBody, macroLocal)
+end
+
+-----------------------------------------------------------------------------------
---
+++ AgisDisit/_README_DE.txt
@@ -0,0 +1,11 @@
+
+Erstelle ein Makro mit folgendem Inhalt:
+
+#showtooltip Entzaubern
+/use [nomod] Entzaubern
+/stopmacro [nomod]
+/disit
+
+Normaler Klick auf das Makro setzt den normalen Entzauberungs-Spell ein
+Mod-Klick (Umschalt/Strg/Alt) entzaubert von selbst das nächste entzauberbare Item
+(nur grüne und blaue und tragbare und verkaufbare Items)
